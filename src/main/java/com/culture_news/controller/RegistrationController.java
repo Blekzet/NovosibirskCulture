1e7f05fa34407bd0c7a72996688153e5eb1e01d8
Blekzet <Fudog177@yandex.ru>
2020-04-17 15:46:08 +0700

    Такие дела... дааа

---
+++ src/main/java/com/culture_news/controller/RegistrationController.java
@@ -0,0 +1,38 @@
+package com.culture_news.controller;
+
+import com.culture_news.entity.User;
+import com.culture_news.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class RegistrationController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model) {
+
+        userService.saveUser(userForm);
+
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+
+        return "redirect:/";
+    }
+}
